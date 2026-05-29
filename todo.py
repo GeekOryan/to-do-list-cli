c0cb37c741d2b139f36d7b3dbbf4277a610e618d
GeekOryan <225357530+GeekOryan@users.noreply.github.com>
2026-05-30 00:30:45 +0200

    Added 'Add' and 'View' Features with sorting by due date and priority

--- todo.py
+++ todo.py
@@ -46,7 +46,23 @@ def main():
         choice = input("Enter your choice (1 - 5): ")
         
         if choice == '1':
-            print("Add task feature : coming soon")
+            task_name = input("Enter task name: ")
+            task_due_date = input("Enter due date (YYYY-MM-DD): ")
+            priority_level = input("Enter priority (High/Medium/Low): ")
+            
+            new_task = {
+                "name": task_name,
+                "due": task_due_date,
+                "priority": priority_level,
+                "status": "pending"
+            }
+            
+            tasks.append(new_task)
+            
+            save_tasks(tasks)
+            
+            print("Task has been added successfully!")
+            
         elif choice == '2':
             if not tasks:
                 print("No tasks found")
@@ -55,24 +71,29 @@ def main():
             
             display_tasks = tasks.copy()
         
-        
+            valid_choice = False
+            
             if sort_choice == '1':
                 display_tasks.sort(key=itemgetter('due'))
+                valid_choice = True
             elif sort_choice == '2':
                 display_tasks.sort(key=itemgetter('priority'))
+                valid_choice = True
             else:
                 print("Invalid input. Please select 1 or 2")
+                continue
                 
-            task_number = 1
+            if valid_choice:
+                task_number = 1
             
-            for display_task in display_tasks:
-                name_value = display_task['name']
-                due_value = display_task['due']
-                priority_value = display_task['priority']
-                status_value = display_task['status']
+                for display_task in display_tasks:
+                    name_value = display_task['name']
+                    due_value = display_task['due']
+                    priority_value = display_task['priority']
+                    status_value = display_task['status']
                 
-                print(f"{task_number}. {name_value} | Due: {due_value} | Priority: {priority_value} | Status: {status_value} ")
-                task_number += 1
+                    print(f"{task_number}. {name_value} | Due: {due_value} | Priority: {priority_value} | Status: {status_value} ")
+                    task_number += 1
         elif choice == '3':
             print("Complete task feature : coming soon")
         elif choice == '4':
